576ca545ca9126470a7ef83f7e0b98b811af5641
lucgiffon <luc.giffon@gmail.com>
2015-10-30 15:47:16 +0100

    suppression des gitignores -> .gitignore.example

---
+++ database/migrations/2015_10_30_140635_create_Participant_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipantTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Event', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            //$table->subtitle('subtitle', 500);
+            $table->string('logo');
+            $table->date('begindate');
+            $table->date('enddate');
+            $table->string('address');
+            $table->string('mailcontact');
+            $table->text('description');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
