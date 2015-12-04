a4982c1f84ce054dbc8233dd617e7f717a975cb7
lucgiffon <luc.giffon@gmail.com>
2015-12-04 12:25:40 +0100

    countrytableseeder debuguée + modele message crée + contact controller commencé

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use DB;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Message;
 
 class ContactController extends Controller
 {
@@ -13,13 +15,63 @@ class ContactController extends Controller
         return view('contact');
     }
 
-    public function postForm()
+    public function postForm(Request $request)
     {
-        if(Request::ajax()) {
-            $data = Input::all();
-            print_r($data);
-            die;
+        $args = array(
+            'name'   => FILTER_SANITIZE_ENCODED,
+            'component'    => array('filter'    => FILTER_VALIDATE_INT,
+                'flags'     => FILTER_REQUIRE_ARRAY,
+                'options'   => array('min_range' => 1, 'max_range' => 10)
+            ),
+            'email'     => FILTER_SANITIZE_ENCODED,
+            'texte' => FILTER_SANITIZE_ENCODED,
+            'testscalar'   => array(
+                'filter' => FILTER_VALIDATE_INT,
+                'flags'  => FILTER_REQUIRE_SCALAR,
+            ),
+            'testarray'    => array(
+                'filter' => FILTER_VALIDATE_INT,
+                'flags'  => FILTER_REQUIRE_ARRAY,
+            )
+
+        );
+         if ($request->isMethod('post')) {
+                 $this->validate($request, [
+                     'nom' => 'required|max:255',
+                     'email' =>'required',
+                     'texte' => 'required'
+                 ]);
+
+             $id = DB::table('Message')->insertGetId([
+                     'name' => $request->input('nom'),
+                     'email' => $request->input('email'),
+                     'description' => $request->input('texte')
+                 ]);
+
+             $message = new Message;
+
+             $message->name = $request->input('nom');
+             $message->email = $request->input('email');
+             $message->description = htmlspecialchars($request->input('texte'));
+
+             $message->save();
+
+             $returned =  DB::table('Message')->where('id', 110)->value('description');
+
+//             $message = new Message([
+//                 'name' => $request->input('nom'),
+//                 'email' => $request->input('email'),
+//                 'description' => $request->input('texte')
+//             ]);
+
+             return var_dump($request->input());
         }
+//        return "coucou";
+//        if(Request::ajax()) {
+//            $data = Input::all();
+//            print_r($data);
+//            die;
+//        }
     }
 
     /**
